6a72b1560d934e3f2fa407812e96b60d489c662d
Gudbrand <gudbrandduff@gmail.com>
2016-02-23 10:02:37 +0700

    Initial Commit

---
+++ Amorous Bugs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amorous Bugs
+//
+//  Created by Gudbrand Tandberg on 23/02/16.
+//  Copyright (c) 2016 Duff Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
